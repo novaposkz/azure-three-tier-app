a461f3b2aa38d2a2ebd73248161a50de95fb790c
novaposkz <tanib@novapos.kz>
2025-09-10 00:44:06 +0500

    Update outputs.tf

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -6,5 +6,5 @@ output "sql_server_fqdn" {
 output "sql_connection_string" {
   description = "SQL connection string"
   value       = "Server=tcp:${azurerm_mssql_server.sql_server.fully_qualified_domain_name},1433;Database=${azurerm_mssql_database.sql_database.name};User ID=${var.admin_username};Password=${random_password.sql_password.result};Encrypt=true;TrustServerCertificate=false;Connection Timeout=30;"
-  sensitive   = true
+  sensitive   = true # ← ДОЛЖНО БЫТЬ sensitive = true (без лишних пробелов!)
 }
